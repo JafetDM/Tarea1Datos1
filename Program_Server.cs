2214a25795d2e127c9d148e9770fe4d1478221fb
JafetDM <148803642+JafetDM@users.noreply.github.com>
2024-07-30 20:35:15 -0600

    Update Program_Server.cs
    instancia de server solicita un puerto

--- Program_Server.cs
+++ Program_Server.cs
@@ -10,15 +10,18 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Server s = new Server("localhost", 4404); //se crea una instancia de un servidor 
+            Console.Write("Ingrese un puerto válido: ");
+            string port = Console.ReadLine();
+            int puerto = int.Parse(port); //convierte la entrada en un int 
+            Server s = new Server("localhost", puerto); //se crea una instancia de un servidor 
             s.Start(); //inicia al servidor para que espere mensaje
             Console.ReadKey();
-            
+
 
             while (true)
             {
                 Console.Write("Esperando mensaje: ");
-                
+
                 Console.ReadKey();
             }
         }
